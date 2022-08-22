69cbe79d2340cdf69f9a31da712be0069062eb19
beebooplena <lena_kath@hotmail.com>
2022-08-22 23:09:04 +0000

    Added app home with index.html template

--- laser_shop/settings.py
+++ laser_shop/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home'
 
 ]
 
@@ -62,7 +63,9 @@ ROOT_URLCONF = 'laser_shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
