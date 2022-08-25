d900be3d78b61031d7aedc3067b4db098ff4a8b7
beebooplena <lena_kath@hotmail.com>
2022-08-25 19:59:54 +0000

    customized admin

--- items/admin.py
+++ items/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
 from .models import Item, Category
 
+class ItemAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'category',
+        'name',
+        'engraved',
+        'price',
+        'discount',
+        'discount_value',
+        'image',
+
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
 # Register your models here.
-admin.site.register(Item)
-admin.site.register(Category)
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Category, CategoryAdmin)
