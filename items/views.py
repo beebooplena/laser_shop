bff1ee61c1e44268072fec56a2191a2b0332bea0
beebooplena <lena_kath@hotmail.com>
2022-08-26 00:57:10 +0000

    Added item_detail template with style

--- items/views.py
+++ items/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Item
 
 # Create your views here.
@@ -13,4 +13,16 @@ def all_items(request):
     }
 
     return render(request, 'items/items.html', RequestContext)
+
+
+def item_detail(request, item_id):
+    """ A view to be able to show one product and it`s details"""
+
+    item = get_object_or_404(Item, pk=item_id)
+
+    context = {
+        'item' : item,
+    }
+
+    return render(request, 'items/item_detail.html',context)
 
