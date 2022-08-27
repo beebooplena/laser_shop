9f08cb351089239bfe6dae98cb7f14dec4e3027e
beebooplena <lena_kath@hotmail.com>
2022-08-27 22:18:57 +0000

    updated contexts.py, testing add bag functionality

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,5 +1,6 @@
 
 from django.conf import settings
+from django.shortcuts import get_object_or_404
 from items.models import Item
 
 def bag_contents(request):
@@ -7,16 +8,37 @@ def bag_contents(request):
     bag_items = []
     total = 0
     item_many = 0
+    delivery = settings.STANDARD_DELIVERY_PRICE
+    bag = request.session.get('bag', {})
 
-  
+    for item_id, amount in bag.items():
+        item = get_object_or_404(Item, pk=item_id)
+        if item.discount is True:
+            total += amount * (item.price - item.discount_value) 
+        else:
+            total += amount * item.price
+            item_many += amount
+       
 
-   
+        bag_items.append({
+            'item_id': item_id,
+            'amount': amount,
+            'item': item,
+
+        })
+
+    sum_total = total + delivery
+
+
     context = {
         'bag_items': bag_items,
         'total': total,
         'item_many': item_many,
+        'delivery': delivery,
+        'sum_total': sum_total,
         
     }
+    print('bag_items')
 
     return context
 
