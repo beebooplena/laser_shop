41d2a0f2444b4d0b9712bf164dff0585d142256b
beebooplena <lena_kath@hotmail.com>
2022-10-09 16:50:27 +0000

    updated function userprofile in views.py

--- userprofiles/views.py
+++ userprofiles/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import CustomerProfile
 
 def userprofile(request):
     """
     Show the customer profile
     """
+    customer = get_object_or_404(CustomerProfile, user=request.user)
     template = 'userprofile/userprofile.html'
-    context = {}
+    context = {
+        'customer': customer,
+    }
 
     return render(request, template, context)
