321aea41c71513852e09f5510d74d1c2b598039e
beebooplena <lena_kath@hotmail.com>
2022-10-10 20:39:33 +0000

    created errors templates, views and urls

--- laser_shop/urls.py
+++ laser_shop/urls.py
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from .views import handler404
+from .views import handler500
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +31,6 @@ urlpatterns = [
     path('userprofile/', include('userprofiles.urls')),
     path('blog/', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'laser_shop.views.handler404'
+handler500 = 'laser_shop.views.handler500'
