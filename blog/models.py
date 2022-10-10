b1afae74b57342271ec247a48cdb09ce93a01a80
beebooplena <lena_kath@hotmail.com>
2022-10-10 15:19:37 +0000

    Added a BlogPost model in blog app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=260)
+    blog_date = models.DateTimeField('Blog Date')
+    blog_image = CloudinaryField('image', default='placeholder')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.TextField()
+
+    def __str__(self):
+        return self.title
 
 # Create your models here.
