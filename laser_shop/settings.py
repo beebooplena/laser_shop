6b453f2879c3c6e332ee66d7ac8dbeff92452599
beebooplena <lena_kath@hotmail.com>
2022-10-25 13:15:55 +0000

    sending real emails part 2

--- laser_shop/settings.py
+++ laser_shop/settings.py
@@ -197,7 +197,7 @@ STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
 STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET')
 STRIPE_CURRENCY = 'usd'
-DEFAULT_FROM_EMAIL = 'lasershop@example.com'
+
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
