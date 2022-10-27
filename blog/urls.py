849ae33204f3dfc6d73a1f0a939dd464aacc91ae
beebooplena <lena_kath@hotmail.com>
2022-10-27 22:14:11 +0000

    Admin blog, adding blog posts functionality

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.show_blog, name='blog')
+    path('', views.show_blog, name='blog'),
+    path('blog_post/<int:pk>/', views.blog_post, name='blog_post'),
+    path('add/', views.adding_post, name='adding_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+
 from .models import BlogPost
+from .forms import BlogForm
 
 
 def show_blog(request):
@@ -11,3 +14,44 @@ def show_blog(request):
     }
 
     return render(request, 'blog/blog.html', context)
+
+
+def blog_post(request, pk):
+    """ This will render blog page """
+    posts = BlogPost.objects.all()
+
+    context = {
+        'posts': posts,
+    }
+
+    return render(request, 'blog/blog_post.html', context)
+
+
+
+def adding_post(request):
+    """
+    Adding items to the webstore
+    """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that')
+        return redirect(reverse('home'))
+    
+
+    if request.method == 'POST':
+        form = BlogForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'You successfully added a blog post')
+            return redirect(reverse('blog'))
+        else:
+            messages.error(request, 'Error. Please make sure that the form is valid.')
+    else:
+        form = BlogForm()
+
+    template = 'blog/add_blog.html'
+    context = {
+        'form': form,
+        
+    }
+
+    return render(request, template, context)
